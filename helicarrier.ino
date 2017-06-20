d170cb6dc7bbd1b7bac7996925c2ce5aee708501
Jamie <jamie@naylorwebsites.co.uk>
2017-06-20 13:11:32 +0100

    tidy up of code and add comments

--- helicarrier.ino
+++ helicarrier.ino
@@ -1,6 +1,6 @@
-int lights = 4;    // LED connected to digital pin 9
-int motorsa = 0;
-int motorsb = 1; 
+int lights = 4;    // LED connected to digital pin 4
+int motorsa = 0;    //pwm pin for motor +
+int motorsb = 1;    //pwm pin for motor -
 int brightness = 0;    // how bright the LED is
 int fadeAmount = 5;    // how many points to fade the LED by
 
@@ -10,19 +10,16 @@ void setup() {
 }
 
 void loop() {
-    analogWrite(lights, brightness);
-    analogWrite(motorsa, brightness);
-    analogWrite(motorsb, 0);
-    // wait for 30 milliseconds to see the dimming effect
-
+  //set values on first loop (should be zero)
+  analogWrite(lights, brightness);
+  analogWrite(motorsa, brightness);
+  analogWrite(motorsb, 0); // sets the motor pin low to act as ground
   // change the brightness for next time through the loop:
   brightness = brightness + fadeAmount;
-
-  // reverse the direction of the fading at the ends of the fade:
+  // once brightness is full just leave it at full:
   if (brightness <= 0 || brightness >= 255) {
-    fadeAmount = -fadeAmount;
+    fadeAmount = fadeAmount;
   }
   // wait for 30 milliseconds to see the dimming effect
   delay(30);
-
 }
